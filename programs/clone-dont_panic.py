c0f48d539622c15e3816d733b098deae15ef5695
Pooja Krishnan <krishnanpooja@users.noreply.github.com>
2021-03-12 13:36:42 +0530

    Create clone-dont_panic.py

---
+++ programs/clone-dont_panic.py
@@ -0,0 +1,37 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+# nb_floors: number of floors
+# width: width of the area
+# nb_rounds: maximum number of rounds
+# exit_floor: floor on which the exit is found
+# exit_pos: position of the exit on its floor
+# nb_total_clones: number of generated clones
+# nb_additional_elevators: ignore (always zero)
+# nb_elevators: number of elevators
+nb_floors, width, nb_rounds, exit_floor, exit_pos, nb_total_clones, nb_additional_elevators, nb_elevators = [int(i) for i in input().split()]
+map=[exit_pos]*nb_floors
+for i in range(nb_elevators):
+    # elevator_floor: floor on which this elevator is found
+    # elevator_pos: position of the elevator on its floor
+    elevator_floor, elevator_pos = [int(j) for j in input().split()]
+    map[elevator_floor]=elevator_pos
+
+# game loop
+while True:
+    inputs = input().split()
+    clone_floor = int(inputs[0])  # floor of the leading clone
+    clone_pos = int(inputs[1])  # position of the leading clone on its floor
+    direction = inputs[2]  # direction of the leading clone: LEFT or RIGHT
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    if clone_pos < map[clone_floor] and direction == "LEFT" or \
+       clone_pos > map[clone_floor] and direction == "RIGHT": 
+       print("BLOCK")
+    # action: WAIT or BLOCK
+    else:
+       print("WAIT")
